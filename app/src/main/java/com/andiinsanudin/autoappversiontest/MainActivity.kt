9142f87060de971d74e29bd84b30f9cea4562c1c
Andi Insanudin <andiisfh@gmail.com>
2018-12-10 10:58:22 +0700

    initial commit

---
+++ app/src/main/java/com/andiinsanudin/autoappversiontest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.andiinsanudin.autoappversiontest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        apk_version_name.text = BuildConfig.VERSION_NAME + " " + BuildConfig.VERSION_CODE
+    }
+}
